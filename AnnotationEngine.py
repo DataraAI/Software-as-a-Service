1a8fb73ae8356fa220448359923f27a1f0a85696
jonathansevilla01 <42389275+jonathansevilla01@users.noreply.github.com>
2026-02-20 10:58:25 -0800

    Annotation engine to modify frames and extract details.

---
+++ AnnotationEngine.py
@@ -0,0 +1,43 @@
+# Imports
+
+
+class AnnotationEngine():
+    # Might use "video" instead of "frame_list"
+    def __init__(self, frame_list):
+        # Create variables
+        self.frame_list = frame_list
+
+        # Call preAnnotation
+        self.preAnnotation()
+
+    # Exocentric frames -> modified frames
+    # Inpainting to remove human, most likely through ROSE
+    def preAnnotation(self):
+        # preAnnotation steps go here
+        # ...
+
+        # Then call inAnnotation
+        self.inAnnotation()
+
+    # Modified frames -> ego frames
+    def inAnnotation(self):
+        # inAnnotation steps go here
+        # ...
+
+        # Then call postAnnotation
+        self.postAnnotation()
+
+    # Egocentric frames -> extracting details about the frames
+    def postAnnotation(self):
+        # postAnnotation steps go here
+        # ...
+
+        # Then return ego + annotations
+        return
+
+
+# Example use case
+# annotation_engine = AnnotationEngine(frame_list)
+
+
+
