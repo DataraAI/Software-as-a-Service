9274cc67c5bb7db7baf29978a9db73e5f319cbe8
Aayush Dubey <aayush@dataraai.ai>
2026-02-28 02:15:57 +0000

    Fixed the segmentation package to get it working with the VM, it handles human masks for now.

--- AnnotationEngine.py
+++ AnnotationEngine.py
@@ -1,9 +1,11 @@
 # Imports
 
 import argparse
+import os
+from PIL import Image
+import numpy as np
 
 import DataraAI_segmentation
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_path", type=str, help="input MP4 video_path")
@@ -26,18 +28,33 @@ class AnnotationEngine():
     # Exocentric frames -> modified frames
     # Inpainting to remove human, most likely through ROSE
     def preAnnotation(self):
-        # preAnnotation steps go here
-        human_masks = DataraAI_segmentation.mask_generation(self.video_path)
+        # sam3 conda environment
+        # os.system("conda activate sam3")
+
+        outputs_per_frame = DataraAI_segmentation.mask_generation(self.video_path)
+
+        # This will write human masks to ~/masks/mask_<frame id>.png
+        # It should take in video input without changing fps
+        i = 0
+        os.makedirs(os.path.join(os.path.expanduser("~"), "masks"), exist_ok=True)
+        for output in outputs_per_frame.values():
+            human_binary_masks = output["out_binary_masks"]
+            # Take only the first human, i.e. human_binary_masks[0]
+            mask = human_binary_masks[0].astype(np.uint8) * 255
+            mask_image = Image.fromarray(mask)
+            mask_image.save(os.path.join(os.path.expanduser("~"), "masks") + "/mask_" + str(i) + '.png')
+            i += 1
+
+        # os.system("conda deactivate")
         # rose inpainting will take in video path and human masks (or similar)
 
     # Modified frames -> ego frames
     def inAnnotation(self):
-        # inAnnotation steps go here
         # ...
+        return
 
     # Egocentric frames -> extracting details about the frames
     def postAnnotation(self):
-        # postAnnotation steps go here
         # ...
 
         # Then return ego + annotations
@@ -46,6 +63,7 @@ class AnnotationEngine():
 
 # Example use case
 annotation_engine = AnnotationEngine(video_path)
-annotation_engine.run()
+# annotation_engine.run()
+annotation_engine.preAnnotation()
 
 
--- README.md
+++ README.md
@@ -9,3 +9,25 @@ python DataraAI_segmentation.py \
     --video_path path/to/video.mp4 \
     --segment "humans"
 ```
+
+## Setup
+
+This is being used on a Ubuntu 24.03 LTS VM, connected to a GH200 GPU with arm64.
+
+### Dependency Issues
+
+#### SAM3
+
+The decord package is not compatible for arm64 systems, so we're replacing it with cv2's VideoCapture.
+
+```python
+# video = VideoReader(video_path, ctx=cpu(0))
+cap = cv2.VideoCapture(video_path)
+frames = []
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frames.append(frame)
+cap.release()
+```
